4d35a54c27166137048327d8c2a8dec613190c3f
V <av@dovercorp.com>
2021-09-02 11:29:41 +0530

    added c# file and text file

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.IO;
+
+namespace OnlyRetrieve
+{
+   
+    public class Student
+    {
+
+        static void Main(string[] args)
+        {
+            string strdata = File.ReadAllText(@"D:\student.txt");
+
+            string[] rowdata = strdata.Split(new[] { "\r\n", "\r", "\n" }, StringSplitOptions.None);
+
+        
+            foreach (string sturecord in rowdata)
+            {
+               
+                string[] splitdata = sturecord.Split('|');
+                Console.WriteLine("student ID:" + splitdata[0]);
+                Console.WriteLine("student name:" + splitdata[1]);
+                Console.WriteLine("student class:" + splitdata[2]);
+                Console.WriteLine("student section:" + splitdata[3] + "\n");
+
+
+            }
+
+        }
+    }
+}
